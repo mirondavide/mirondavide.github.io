bf6ed38a783d3251514a613d397a46d8a267be95
mirondavide <davidemiron30@gmail.com>
2025-04-11 01:03:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 let actualMode = "darkMode";
 
+const img = document.getElementById("logo");
 const personalProjects = document.getElementById("personalProjects");
 const h1 = document.getElementsByTagName("h1");
 const p = document.getElementsByTagName("p");
 const backCard = document.getElementById("backCard");
 const frontCard = document.getElementById("frontCard");
+const Button = document.getElementsByTagName("button");
 const logo = document.getElementById("logo");
-const curriculum = document.getElementById("curriculum");
 
 function changeTheme() {
-  const isDark = actualMode === "darkMode";
+  if (actualMode === "darkMode") {
+    for (let i = 0; i < p.length; i++) {
+      p[i].style.color = "black";
+    }
 
-  for (let i = 0; i < p.length; i++) {
-    p[i].style.color = isDark ? "black" : "white";
-  }
+    for (let i = 0; i < h1.length; i++) {
+      h1[i].style.color = "black";
+    }
 
-  for (let i = 0; i < h1.length; i++) {
-    h1[i].style.color = isDark ? "black" : "white";
-  }
+    personalProjects.style.color = "black";
+    document.body.style.backgroundColor = "white";
+    frontCard.style.backgroundColor = "white";
+    backCard.style.backgroundColor = "white";
+    logo.src = "imgLogoInverted.png";
 
-  personalProjects.style.color = isDark ? "black" : "white";
-  document.body.style.backgroundColor = isDark ? "white" : "black";
-  frontCard.style.backgroundColor = isDark ? "white" : "black";
-  backCard.style.backgroundColor = isDark ? "white" : "black";
-  logo.src = isDark ? "imgLogoInverted.png" : "logo.jpg.png";
+    actualMode = "lightMode";
+  } else {
+    for (let i = 0; i < p.length; i++) {
+      p[i].style.color = "white";
+    }
 
-  actualMode = isDark ? "lightMode" : "darkMode";
+    for (let i = 0; i < h1.length; i++) {
+      h1[i].style.color = "white";
+    }
 
- if(actualMode == "lightMode")
-  {
-    curriculum.addEventListener('mouseover',() =>{
-    curriculum.style.backgroundColor = "#424442";    
-    });
+    personalProjects.style.color = "white";
+    document.body.style.backgroundColor = "black";
+    frontCard.style.backgroundColor = "black";
+    backCard.style.backgroundColor = "black";
+    logo.src = "logo.jpg.png";
+
+    actualMode = "darkMode";
   }
 }
