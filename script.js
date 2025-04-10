d651e50201b5c7e582478b8f4db5936798f6c719
mirondavide <davidemiron30@gmail.com>
2025-04-10 23:43:12 +0200

    Update script.js con modalita tema

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let actualMode = "darkMode";
+
 const img = document.getElementById("logo");
 const personalProjects = document.getElementById("personalProjects");
 const h1 = document.getElementsByTagName("h1");
@@ -5,18 +7,40 @@ const p = document.getElementsByTagName("p");
 const backCard = document.getElementById("backCard");
 const frontCard = document.getElementById("frontCard");
 const Button = document.getElementsByTagName("button");
+const logo = document.getElementById("logo");
 
 function changeTheme() {
-  for (let i = 0; i < p.length; i++) {
-    p[i].style.color = "black";
-  }
+  if (actualMode === "darkMode") {
+    for (let i = 0; i < p.length; i++) {
+      p[i].style.color = "black";
+    }
 
-  for (let i = 0; i < h1.length; i++) {
-    h1[i].style.color = "black";
-  }
+    for (let i = 0; i < h1.length; i++) {
+      h1[i].style.color = "black";
+    }
 
-  personalProjects.style.color = "black";
-  document.body.style.backgroundColor = "white";
-  frontCard.style.backgroundColor = "white";
-  backCard.style.backgroundColor = "white";
+    personalProjects.style.color = "black";
+    document.body.style.backgroundColor = "white";
+    frontCard.style.backgroundColor = "white";
+    backCard.style.backgroundColor = "white";
+    logo.src = "imgLogoInverted.png";
+
+    actualMode = "lightMode";
+  } else {
+    for (let i = 0; i < p.length; i++) {
+      p[i].style.color = "white";
+    }
+
+    for (let i = 0; i < h1.length; i++) {
+      h1[i].style.color = "white";
+    }
+
+    personalProjects.style.color = "white";
+    document.body.style.backgroundColor = "black";
+    frontCard.style.backgroundColor = "black";
+    backCard.style.backgroundColor = "black";
+    logo.src = "logo.jpg.png";
+
+    actualMode = "darkMode";
+  }
 }
